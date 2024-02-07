5051dd695a54fd1eeeaaec119c2fbcbaeb637fb3
しょうご <155814857+Shogo-Kai@users.noreply.github.com>
2024-02-08 00:18:21 +0900

    スレッドの一覧表示

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [thread, setThread] = useState([])
+
+    //useEffectについてよく分からない　調べる
+    useEffect(() => {
+        fetch('https://railway.bulletinboard.techtrain.dev/threads?offset=0')
+        .then(res => res.json())
+        .then(data => {setThread(data)})
+    }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+       {thread.map((thread) => <li key={thread.id}>{thread.title}</li>)}
+      </ul>
     </div>
   );
 }
