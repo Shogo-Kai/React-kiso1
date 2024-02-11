40f39e0341f577dc4c6df322ec5fd031fa89a7b2
しょうご <155814857+Shogo-Kai@users.noreply.github.com>
2024-02-11 10:47:05 +0900

    react-loader-spinnerの活用

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,28 @@
 import './App.css';
 import { useEffect, useState } from 'react';
+import { DNA } from 'react-loader-spinner';
+
 
 function App() {
   const [thread, setThread] = useState([])
+  const [loading, setLoading] =useState(false)
 
     useEffect(() => {
+
       try {
-        fetch('https://railway.bulletinboard.techtrain.dev/threads?offset=0')
+
+        setTimeout(() => {
+          fetch('https://railway.bulletinboard.techtrain.dev/threads?offset=0')
         .then(res => res.json())
         .then(data => {setThread(data)})
+        .then(() => setLoading(true))}, 5000);
+
       }catch(error) {
+
         alert("APIからデータを取得できませんでした。");
+
       }
+
     }, [])
 
   return (
@@ -26,7 +37,7 @@ function App() {
       <div className="center">
         <ul>
           <li className="head">新着スレッド</li>
-          {thread.map((thread) => <li key={thread.id}><div className="box">{thread.title}</div></li>)}
+          {loading? thread.map((thread) => <li key={thread.id}><div className="box">{thread.title}</div></li>) : <DNA height="800" width="800"/>}
         </ul>
       </div>
     </main>
