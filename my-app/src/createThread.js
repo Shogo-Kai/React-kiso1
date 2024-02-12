02b82c60579488746db5d1d71f3ce880288f74ae
しょうご <155814857+Shogo-Kai@users.noreply.github.com>
2024-02-12 17:59:03 +0900

    画面遷移機能の実装

---
+++ my-app/src/createThread.js
@@ -0,0 +1,23 @@
+function createThread() {
+  
+    return (
+      <>
+      <header>
+        <div className='left_a_right'>
+          <div className='left'>掲示板</div>
+          <div className='right'><a href=''>スレッドをたてる</a></div>
+        </div>
+      </header>
+      <main>
+        <div className="center">
+          <ul>
+            <li className="head">スレッド新規作成</li>
+          </ul>
+        </div>
+      </main>
+      </>
+    );
+  }
+  
+  export default createThread;
+  
